11d5ddf430911d0bdca18644888677641350806f
Diego Freire <hig200diego@gmail.com>
2019-03-26 14:47:16 -0300

    Script usando o switch para pegar os dias da semana

--- horas/horas.js
+++ horas/horas.js
@@ -1,4 +1,46 @@
 
+switch(new Date().getDay()) {
+    case 0:
+    day = "Sunday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+
+    case 1:
+    day = "Monday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+
+    case 2:
+    day = "Tuesday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+
+    case 3:
+    day = "Wednesday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+
+    case 4:
+    day = "Thursday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+    
+    case 5:
+    day = "Friday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+    
+    case 6:
+    day = "Saturday";
+    document.getElementById("mostra-dia-semana").innerHTML = day;
+    break;
+
+    default:
+    mensagem = "Looking forward to the Weekend";
+
+
+}
+
 
 function horario() {
     var hora = new Date().getHours();
